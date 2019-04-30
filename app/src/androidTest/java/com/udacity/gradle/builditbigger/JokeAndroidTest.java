48066958ba331977b465c17aadb4fabb17425b0f
Maja Francetic <francetic.maja@gmail.com>
2019-04-30 17:16:51 +0200

    Junit dependencies added to the JokeDisplayLibrary

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/JokeAndroidTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/JokeAndroidTest.java
@@ -1,4 +1,5 @@
 package com.udacity.gradle.builditbigger;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,28 +10,35 @@ import static junit.framework.Assert.assertNotSame;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.util.Log;
+
 import java.util.concurrent.TimeUnit;
 
 @RunWith(AndroidJUnit4.class)
 public class JokeAndroidTest {
 
+    /* Tag for log messages */
+    private final static String LOG_TAG = JokeAndroidTest.class.getSimpleName();
+
     @Rule
     public final ActivityTestRule<MainActivity> activityTestRule =
             new ActivityTestRule<>(MainActivity.class);
 
+    /**
+     * Test that checks if the EndpointsAsyncTask returns a valid String
+     */
     @Test
     public void asyncTask_returnsString() {
-
+        /* Execute the endpointsAsyncTask */
         EndpointsAsyncTask endpointsAsyncTask = new EndpointsAsyncTask();
-
         endpointsAsyncTask.execute();
 
+        /* Check if the result from the endpointsAsyncTask is null or empty */
         try {
             String result = endpointsAsyncTask.get(30, TimeUnit.SECONDS);
             assertNotNull(result);
             assertNotSame("", result);
         } catch (Exception e) {
-            Log.e("JokeAndroidTest", "Timed out" + e);
+            Log.e(LOG_TAG, "Timed out" + e);
         }
     }
 }
