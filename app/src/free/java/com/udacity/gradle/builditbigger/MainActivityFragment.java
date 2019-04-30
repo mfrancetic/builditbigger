6f12239c8aedb188533c4eb3f5ae74796e273e2a
Maja Francetic <francetic.maja@gmail.com>
2019-04-30 17:34:27 +0200

    Internalization implemented - added strings for Croatian language

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,24 +1,15 @@
 package com.udacity.gradle.builditbigger;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.InterstitialAd;
 import com.google.android.gms.ads.MobileAds;
-import com.udacity.joketellinglibrary.JokeTellingClass;
 
-
-/**
- * A placeholder fragment containing a simple view.
- */
 public class MainActivityFragment extends Fragment {
 
     public MainActivityFragment() {
@@ -27,32 +18,23 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        /* Inflate the view */
         View root = inflater.inflate(R.layout.fragment_main, container, false);
 
-        MobileAds.initialize(getActivity().getApplicationContext(), getString(R.string.banner_ad_id));
+        if (getActivity() != null) {
+            /* Initialize a mobile ad */
+            MobileAds.initialize(getActivity().getApplicationContext(), getString(R.string.banner_ad_id));
+        }
 
-        AdView mAdView = (AdView) root.findViewById(R.id.adView);
+        /* Find the adView and make it visible */
+        AdView mAdView = root.findViewById(R.id.adView);
         mAdView.setVisibility(View.VISIBLE);
 
-        // Create an ad request. Check logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
-
-//        String deviceIdEmulator = "B3EEABB8EE11C2BE770B684D95219ECB";
+        /* Create and build an ad request.  */
         AdRequest adRequest = new AdRequest.Builder()
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-//                .addTestDevice(deviceIdEmulator)
-
                 .build();
         mAdView.loadAd(adRequest);
-
-//        InterstitialAd interstitialAd = new InterstitialAd(getActivity());
-//
-//        interstitialAd.setAdUnitId(getString(R.string.banner_ad_id));
-//
-
-
         return root;
     }
-
 }
